05e60a59ad61f119d8278bab66582a41f2bb7358
Chun-Yu Lin <chunyu612016@gmail.com>
2024-10-02 22:01:51 +0800

    feat: using langchain for chroma db operation

--- chromaDB.py
+++ chromaDB.py
@@ -1,50 +1,55 @@
-from chromadb.utils import embedding_functions
-from chromadb.config import Settings
-from dotenv import load_dotenv
-import chromadb
 import os
+
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+import chromadb
+from chromadb.config import Settings
 
 # get user & pwd from env
 load_dotenv()
-chromadb_host = os.getenv('chromadb_host')
-chromadb_user = os.getenv('chromadb_user')
-chromadb_pwd = os.getenv('chromadb_password')
+chromadb_host = os.getenv("chromadb_host")
+chromadb_user = os.getenv("chromadb_user")
+chromadb_pwd = os.getenv("chromadb_password")
 
 # DB connection using base auth
 chroma_client = chromadb.HttpClient(
     settings=Settings(
         chroma_client_auth_provider="chromadb.auth.basic_authn.BasicAuthClientProvider",
-        chroma_client_auth_credentials= f'{chromadb_user}:{chromadb_pwd}'
-        ),
-    host=chromadb_host, port=7878
+        chroma_client_auth_credentials=f"{chromadb_user}:{chromadb_pwd}",
+    ),
+    host=chromadb_host,
+    port=7878,
 )
 
 # if everything is correctly configured the below should list all collections
 if chroma_client.list_collections():
-    print('connected')
+    print("connected")
 else:
-    print('connection error')
+    print("connection error")
 
+# Define the HuggingFace embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-
-# define the embedding model
-sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-
-# First time use create_collection
-# collection = chroma_client.create_collection(name="test_collection", embedding_function=sentence_transformer_ef)
-collection = chroma_client.get_collection(name="test_collection", embedding_function=sentence_transformer_ef)
-
-# test case
-collection.add(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ],
-    ids=["id1", "id2"]
+# Setup Chroma client through Langchain
+vector_store = Chroma(
+    collection_name="test_collection",
+    embedding_function=embeddings,
+    client=chroma_client,
+    create_collection_if_not_exists=True,
 )
 
-results = collection.query(
-    query_texts=["This is a query document about pineapple"], # Chroma will embed this for you
-    n_results=2 # how many results to return
+# # test case
+# vector_store.add_texts(
+#     texts=["This is a document about bananas", "This is a document about great tits"],
+#     metadatas = [{"id": "id1", "ref":"test_set"}, {"id": "id2", "ref":"test_set"}], # optional
+#     # ids=["id1", "id2"] # optional
+# )
+
+# query the vector store
+results = vector_store.similarity_search_with_relevance_scores(
+    query="This is a query document about pineapple",
+    k=4,  # how many results to return
 )
 print(results)
